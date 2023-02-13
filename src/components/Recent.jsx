997a6cd0e102dc0cb7cd747bcbec6ef85b503936
Nilay Singh <snilay011@gmail.com>
2023-02-14 03:21:56 +0530

    Recent Blogs Component Completed

--- src/components/Recent.jsx
+++ src/components/Recent.jsx
@@ -2,18 +2,37 @@ import React from 'react'
 import styled from 'styled-components'
 import RecentCard from './RecentCard';
 
-const Container = styled.div``;
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+gap: 1.5rem;
+`;
 
-const Heading = styled.p``;
+const Heading = styled.p`
+font-size: 2rem;
+color:#0ea5ea;
+font-weight: bold;
+`;
 
-const RecentBlogContainer = styled.div``;
+const RecentBlogContainer = styled.div`
+display:flex;
+flex-direction: column;
+gap: 1rem;
+`;
 
 
 
 const Recent = () => {
   return (
       <Container>
+          <Heading>Recent Blogs</Heading>
           <RecentBlogContainer>
+               <RecentCard/>
+               <RecentCard/>
+               <RecentCard/>
+               <RecentCard/>
+               <RecentCard/>
+               <RecentCard/>
                <RecentCard/>
           </RecentBlogContainer>
       </Container>
