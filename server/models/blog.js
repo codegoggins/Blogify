f29ae18bf5a8f1f126f6b02fbe65cef370c10eb4
Nilay Singh <snilay011@gmail.com>
2023-03-08 22:48:31 +0530

    Blog Model Created

---
+++ server/models/blog.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const BlogSchema = new mongoose.Schema({
+    userId:{
+        type:String,
+        required:true
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    desc:{
+        type:String,
+        required:true
+    },
+    blogImg:{
+        type:String,
+        required:true
+    },
+    tags:{
+        type:[String],
+        default:[]
+    },
+    likes:{
+        type:[String],
+        default:[]
+    },
+    dislikes:{
+        type:[String],
+        default:[]
+    }
+},{timestamps:true});
+
+export default mongoose.model("Blog",BlogSchema);
