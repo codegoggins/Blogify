da23c31f3b5989296838a23588861d5e505d6623
Nilay Singh <snilay011@gmail.com>
2023-03-09 18:22:52 +0530

    Sign In Route Completed using Access Tokens

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,8 +1,9 @@
 // AUTHENTICATION
 
-import mongoose from 'mongoose';
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
+import { createError } from '../error.js';
+import jwt from 'jsonwebtoken';
 
 // SIGN UP FUNCTION
 export const signup = async (req,res,next) => {
@@ -20,4 +21,29 @@ export const signup = async (req,res,next) => {
 }
 
 // SIGN IN FUNCTION
-export const signin = async (req,res) => {}
+export const signin = async (req,res,next) => {
+
+    try{
+    
+        const user = await User.findOne({name:req.body.name});
+        if(!user){
+            return next(createError(404,"User Not Found"));
+        }
+
+        const check = await bcrypt.compare(req.body.password,user.password);
+
+        if(!check){
+            return next(createError(400,"Wrong Credentials"));
+        }
+
+        const token = jwt.sign({id:user._id},process.env.JWT);
+        const {password , ...others} = user;
+
+        return res.cookie("access_token",token,{
+            httpOnly:true
+        }).status(200).json(others._doc);
+
+    }catch(err){
+        next(err);
+    }
+}
