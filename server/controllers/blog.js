b2b8331443abc7e25618c5d92a4dd944af5b4ab9
Nilay Singh <snilay011@gmail.com>
2023-03-09 23:07:46 +0530

    Routes Add for Blogs

---
+++ server/controllers/blog.js
@@ -0,0 +1,24 @@
+// CREATE A BLOG
+export const createBlog = async (req,res,next) => {
+
+}
+
+// GET A BLOG
+export const getBlog = async (req,res,next) => {
+
+}
+
+//GET ALL BLOGS
+export const getAllBlogs = async (req,res,next) => {
+
+}
+
+//DELETE A BLOG
+export const deleteBlog = async (req,res,next) => {
+
+}
+
+//UPDATE A BLOG
+export const updateBlog = async (req,res,next) => {
+
+}
--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,5 +1,23 @@
 import express from 'express';
+import { verifyToken } from '../verifyToken.js';
+import {createBlog,updateBlog,deleteBlog,getBlog,getAllBlogs} from '../controllers/blog.js'
 
 const router = express.Router();
+
+// CREATE A BLOG
+router.post('/',verifyToken,createBlog);
+
+// GET A BLOG
+router.get('/:id',getBlog);
+
+// GET ALL BLOGS
+router.get('/',getAllBlogs);
+
+// DELETE A BLOG
+router.delete('/:id',verifyToken,deleteBlog);
+
+// UPDATE A BLOG
+router.put('/:id',verifyToken,updateBlog);
+
 
 export default router;
