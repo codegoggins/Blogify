b30ec4d7beccf8b06a35473970a516dd318c365a
Nilay Singh <snilay011@gmail.com>
2023-03-19 02:58:07 +0530

    Fetch Most Liked Posts Feature Complete

--- client/src/components/Featured.jsx
+++ client/src/components/Featured.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PostCard from './PostCard'
 import styled from 'styled-components'
+import axios from 'axios';
 
 
 const Featured = () => {
+
+  const [blogs,setBlogs] = useState([]);
+  const [error,setError] = useState(false);
+
+  useEffect(()=>{
+    const fetchBlogs = async () => {
+       try{
+        const response = await axios.get('/blogs/trending');
+        setBlogs(response.data);
+       }catch(err){
+          setError(true);
+       }
+    }
+    fetchBlogs();
+  },[]);
+
   return (
     <Container>
         <Heading>Featured</Heading>
     <CardContainer>
-        <PostCard/>
-        <PostCard/>
-        <PostCard/>
-        <PostCard/>
-        <PostCard/>
+    {
+      blogs.map((blog)=>(
+        <PostCard key={blog._id} blog={blog}/>
+      ))
+    }
     </CardContainer>
     </Container>
   )
@@ -31,11 +48,11 @@ gap: 1rem;
 const CardContainer = styled.div`
 display: grid;
 padding: 1rem;
-grid-template-columns: 1fr 1fr 1fr;
+grid-template-columns:repeat(3,1fr);
 gap: 1.5rem;
 
 @media(max-width:768px){
-  grid-template-columns: 1fr;
+  grid-template-columns:100%;
 }
 
 `;
